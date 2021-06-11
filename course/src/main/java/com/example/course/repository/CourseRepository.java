4abb334325792bf8fd7e8002227cf2fcbb93c01b
Malakaba <83619746+Malakaba@users.noreply.github.com>
2021-06-10 21:08:18 -0300

    delete project

--- course/src/main/java/com/example/course/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.course.repository;
-
-import com.example.course.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
